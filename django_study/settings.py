f1342cb662f158476133909414f73529050b7361
abrorbek_anvarbekov <1aabror@gmail.com>
2023-04-12 23:06:19 +0900

    asdas

--- django_study/settings.py
+++ django_study/settings.py
@@ -127,7 +127,9 @@ STATIC_URL = '/static/'
 
 # STATIC_DIR=os.path.join(BASE_DIR,'static')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # STATIC_ROOT=os.path.join(BASE_DIR,'.static')
 
