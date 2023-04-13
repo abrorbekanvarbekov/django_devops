3f977b107aa64d51feeb379bd671f2f20899941e
abrorbek_anvarbekov <1aabror@gmail.com>
2023-04-13 13:35:03 +0900

    db 변경하기

--- django_study/settings.py
+++ django_study/settings.py
@@ -81,13 +81,24 @@ WSGI_APPLICATION = 'django_study.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '18.183.88.241',
+        'NAME': 'abror-db',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'PORT': '3306',
+        'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
